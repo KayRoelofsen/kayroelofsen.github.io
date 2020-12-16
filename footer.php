21d262f190ae80142c1a8a8b6db8b4d3522e0c4d
KayRoelofsen <kay2012@live.nl>
2020-12-16 18:10:48 +0100

    e

---
+++ footer.php
@@ -0,0 +1,109 @@
+<footer>
+<div class="footer">
+    <div class="container wide">
+        <div class="row">
+            <div class="col-md-12 mx-auto footer-news">
+                <h2>
+                    Want us to email you occasionally?
+                </h2> 
+               <input type="email" name="news" id="news"> <button> Subscribe </button>
+            </div>
+            <div class="col-md-6">
+               <h4>tekst</h4>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum enim at dui fermentum convallis. Nullam tristique massa id libero efficitur rutrum. Curabitur sed quam imperdiet, dignissim justo non, varius leo. Mauris blandit aliquet blandit. Aliquam erat volutpat. Sed id nibh bibendum, tincidunt mi eget, rhoncus purus. Aliquam imperdiet lectus molestie pretium sagittis. 
+            </div>
+            <div class="col-md-2">
+                <h5>Links</h5>
+                <ul>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5>Links</h5>
+                <ul>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5>Links</h5>
+                <ul>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                    <li>
+                        <a href="/">link</a>
+                    </li>
+                </ul>
+            </div>
+         
+
+        </div>
+        <hr/>
+        <p style="text-align:center;"> © test 2020. All rights reserved. </p>
+    </div>
+</div>
+</footer>
+
+
+
+<style>
+.footer{background:#205dc5;}
+.footer .container{padding:50px; padding-bottom:5px;}
+.footer-news{text-align:center; margin:5%; }
+.footer-news h2{font-weight:100!important; color:#fff;}
+
+#news{    
+    border-radius: 15px;
+    width: 25%;
+    border: 0px;
+    padding: 0.5rem;
+    }
+    button{
+        border-radius: 15px;
+    background: #328af1;
+    border: 0px;
+    padding: 0.5rem;
+    color: white;
+    margin-left: -80px;
+    }
+hr{background:#fff;}
+
+    .footer{color:#fff;}
+</style>
---
+++ header.php
@@ -0,0 +1,34 @@
+
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css?ver=<?php echo rand(111,999)?>">
+<link rel = "icon" href = "favicon/favicon.png"> 
+<link rel=“stylesheet” href=“https://use.fontawesome.com/releases/v5.2.0/css/all.css” integrity=“sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU” crossorigin=“anonymous”><link rel="stylesheet" type="text/css" href="/wp-content/themes/d-fokker-child/css/main.css?ver=<?php echo rand(111,999)?>">
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+
+<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+
+
+
+<div id="desktop-menu">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/school/">test design</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        
+      </ul>
+    </div>
+  </nav>
+</div>
